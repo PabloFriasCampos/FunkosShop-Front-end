53b0b86e6851ce1c8429b6bcbf733506c38a4f79
PabloFriasCampos <p4blule@gmail.com>
2024-01-30 23:14:09 +0100

    Inicio de sesión (Sin Token JSWT)

--- FunkosShopFront-end/src/app/Sesion/inicio-sesion/inicio-sesion.component.ts
+++ FunkosShopFront-end/src/app/Sesion/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
+
+  usuario: Usuario = new Usuario;
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  async iniciarSesion() {
+    const headers = { 'Content-Type': `application/json` };
+
+    let request$ = await this.http.post("https://localhost:7281/Usuario/login", JSON.stringify(this.usuario), { headers });
+    const correcto = await lastValueFrom(request$);
+
+    if (correcto === true) {
+      this.router.navigate([''])
+      alert("Inicio de sesión correcto")
+    } else {
+      alert("Usuario o contraseña erróneos");
+    }
+
+  }
 
 }
--- FunkosShopFront-end/src/app/Sesion/registrarse/registrarse.component.ts
+++ FunkosShopFront-end/src/app/Sesion/registrarse/registrarse.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Usuario } from 'src/app/model/usuario';
 
@@ -13,14 +14,15 @@ export class RegistrarseComponent {
   usuario: Usuario = new Usuario;
   contrasena: string = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   async registrarUsuario() {
     const headers = { 'Content-Type': `application/json` };
 
     if (this.contrasena == this.usuario.Contrasena) {
-      let request$ = await this.http.post("https://localhost:7281/Usuario", JSON.stringify(this.usuario), { headers });
+      let request$ = await this.http.post("https://localhost:7281/Usuario/signup", JSON.stringify(this.usuario), { headers });
       await lastValueFrom(request$);
+      this.router.navigate(['/login'])
 
     } else {
       alert("Las contraseñas deben ser iguales");
