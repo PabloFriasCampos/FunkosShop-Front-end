c261f18a18c55a9b568f12a6c7886d16fc65de02
David Ortiz Corchero <david140303@hotmail.es>
2024-02-23 17:44:53 +0100

    Arreglos e implementación de modificar contraseña y correo

--- FunkosShopFront-end/src/app/Usuario/datos-usuario/datos-usuario.component.ts
+++ FunkosShopFront-end/src/app/Usuario/datos-usuario/datos-usuario.component.ts
@@ -9,19 +9,20 @@ import { Usuario } from 'src/app/model/usuario';
   templateUrl: './datos-usuario.component.html',
   styleUrls: ['./datos-usuario.component.css']
 })
-export class DatosUsuarioComponent implements OnInit{
+export class DatosUsuarioComponent implements OnInit {
   cuentaUser: Usuario = new Usuario();
   modificarNameUser: string = "";
   modificarEmailUser: string = "";
   modificarContrasenaUser: string = "";
+  confirmarContrasena: string = "";
   modificarDireccionUser: string = "";
   usuarioID: string = "";
 
   constructor(private http: HttpClient, private totalCarrito: TotalCarritoService) { }
 
-  async ngOnInit(): Promise<void>{
+  async ngOnInit(): Promise<void> {
     this.usuarioID = sessionStorage.getItem('usuarioID') ?? localStorage.getItem('usuarioID') ?? "";
-    const request$ = await this.http.get("https://localhost:7281/api/Usuarios/"  + this.usuarioID)
+    const request$ = await this.http.get("https://localhost:7281/api/Usuarios/" + this.usuarioID)
     this.cuentaUser = await lastValueFrom(request$) as Usuario;
     console.log(this.cuentaUser)
   }
@@ -36,10 +37,10 @@ export class DatosUsuarioComponent implements OnInit{
   }
 
   async modificarNombre() {
-    if (this.cuentaUser.nombreUsuario != this.modificarNameUser && this.modificarNameUser != "") {
-      const headers = {'Content-Type':'application/json'};
-  
-      const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarUser/" + this.usuarioID,  JSON.stringify(this.modificarNameUser) , {headers});
+    if (this.cuentaUser.nombreUsuario != this.modificarNameUser && this.modificarNameUser != "" || this.modificarNameUser.trim.length > 0) {
+      const headers = { 'Content-Type': 'application/json' };
+
+      const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarUser/" + this.usuarioID, JSON.stringify(this.modificarNameUser), { headers });
       await lastValueFrom(request$)
 
       alert("El nombre de usuario ha sido modificado con exito");
@@ -48,30 +49,31 @@ export class DatosUsuarioComponent implements OnInit{
     }
   }
 
-  async modificarEmail() { // Sin terminar porque da error
-    if (this.cuentaUser.correo != this.modificarEmailUser && this.modificarEmailUser != "") { // Comprueba que el correo que se ha metido no sea el mismo que el que tiene
-        // Comprobar que el correo no está ya registrado
-        const headers = {'Content-Type':'application/json'};
-        const verificarEmail$ = await this.http.get("https://localhost:7281/api/Usuarios/verificarEmail/" + this.modificarEmailUser, {headers});
-        const existeEmail = await lastValueFrom(verificarEmail$);
-        
-        if (existeEmail == false) {
-            const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarEmail/" + this.usuarioID,  JSON.stringify(this.modificarEmailUser) , {headers});
-            await lastValueFrom(request$);
-            alert("El correo ha sido modificado con éxito");
-        } else {
-            alert("Ya existe una cuenta asociada a ese correo. Por favor selecciona otro correo electrónico");
-        }
-    } else {
-        alert("Estás poniendo el mismo email o el campo está vacío");
-    }
-}
+  async modificarEmail() {
+    if (this.cuentaUser.correo != this.modificarEmailUser && this.modificarEmailUser != "" || this.modificarEmailUser.trim.length > 0) { // Comprueba que el correo que se ha metido no sea el mismo que el que tiene
+      // Comprobar que el correo no está ya registrado
+      const headers = { 'Content-Type': 'application/json' };
+      const verificarEmail$ = await this.http.get("https://localhost:7281/api/Usuarios/verificarEmail/" + this.modificarEmailUser, { headers });
+      const existeEmail = await lastValueFrom(verificarEmail$);
+      // console.log("Existe email "+existeEmail);
 
-  async modificarContrasena() { // Sin terminar porque da error
-    if (this.cuentaUser.contrasena != this.modificarContrasenaUser && this.modificarContrasenaUser != "") {
-      const headers = {'Content-Type':'application/json'};
-  
-      const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarContrasena/" + this.usuarioID,  JSON.stringify(this.modificarContrasenaUser) , {headers});
+      if (existeEmail == false) {
+        const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarEmail/" + this.usuarioID, JSON.stringify(this.modificarEmailUser), { headers });
+        await lastValueFrom(request$);
+        alert("El correo ha sido modificado con éxito");
+      } else {
+        alert("Ya existe una cuenta asociada a ese correo. Por favor selecciona otro correo electrónico");
+      }
+    } else {
+      alert("Estás poniendo el mismo email o el campo está vacío");
+    }
+  }
+
+  async modificarContrasena() {
+    if (this.cuentaUser.contrasena != this.modificarContrasenaUser && this.modificarContrasenaUser != "" && this.modificarContrasenaUser == this.confirmarContrasena || this.modificarContrasenaUser.trim.length > 0) {
+      const headers = { 'Content-Type': 'application/json' };
+
+      const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarContrasena/" + this.usuarioID, JSON.stringify(this.modificarContrasenaUser), { headers });
       await lastValueFrom(request$)
 
       alert("La contraseña ha sido modificada con exito");
@@ -81,10 +83,10 @@ export class DatosUsuarioComponent implements OnInit{
   }
 
   async modificarDireccion() {
-    if (this.cuentaUser.direccion != this.modificarDireccionUser && this.modificarDireccionUser != "") {
-      const headers = {'Content-Type':'application/json'};
-  
-      const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarDireccion/" + this.usuarioID,  JSON.stringify(this.modificarDireccionUser) , {headers});
+    if (this.cuentaUser.direccion != this.modificarDireccionUser && this.modificarDireccionUser != "" || this.modificarDireccionUser.trim.length > 0) {
+      const headers = { 'Content-Type': 'application/json' };
+
+      const request$ = await this.http.put("https://localhost:7281/api/Usuarios/modificarDireccion/" + this.usuarioID, JSON.stringify(this.modificarDireccionUser), { headers });
       await lastValueFrom(request$)
 
       alert("El correo ha sido modificado con exito");
